59182e2e5af5da97011ca603d3d3086dfdf9aebb
umadatt1972 <54751111+umadatt1972@users.noreply.github.com>
2022-07-20 22:06:23 +0530

    Initial commit

---
+++ src/main/java/com/example/demo4/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.example.demo4.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+	
+	@GetMapping("/display")
+	public String checkDisplay() {
+		return "Display is working in Demo Controller";
+	}
+
+}
